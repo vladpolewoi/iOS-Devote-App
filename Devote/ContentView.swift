28697f360708dac60e4aa96a9cb7039ae9baf79d
Vlad Polevoi <vlad.polewoi1@gmail.com>
2023-12-05 17:33:27 +0200

    Initial Commit

---
+++ Devote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Devote
+//
+//  Created by Quest76 on 05.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Devote/DevoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Quest76 on 05.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
